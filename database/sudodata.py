d0c4d6c08593d5ec398d0e336b42d4bbf3eb1893
lollyluan <89143677+lollyluan@users.noreply.github.com>
2024-05-08 17:45:00 +1000

    upload sudo data to es

--- database/sudodata.py
+++ database/sudodata.py
@@ -1,12 +1,18 @@
 from elasticsearch import Elasticsearch
 from elasticsearch.helpers import bulk
+from dotenv import load_dotenv
 import os
+import json
 
-es = Elasticsearch(
+# Connect to Elasticsearch using username and password
+es_client = Elasticsearch(
     "https://127.0.0.1:9200/",
     #api_key = "b9c59b70-88fb-44d5-b00b-ba18f87a3970"
     verify_certs=False,
     basic_auth = ("elastic","elastic"))
+
+INDEX_NAME = "sudo"
+CHUNK_SIZE = 500
 
 # Define the mappings for sudo index
 def create_index():
@@ -53,8 +59,30 @@ def create_index():
         }
     }
 
-    response = es.indices.create(index="sudo", body=mapping)
+    response = es_client.indices.create(index=INDEX_NAME, body=mapping)
     return response
 
+# Upload sudo regional and population data to elasticsearch
 def insert_sudo_data():
+    # Open json and load
+    with open("../data/SUDO-ABS-PopulationDensity/sudo_region.json", "r") as json_file:
+        json_data = json.load(json_file)
+       
+    total_index = 0
+    documents = []    
     
+    for index, doc in enumerate(json_data):
+        # Assign a ID to each document
+        doc["_id"] = index
+        documents.append(doc)
+
+        # Sending amount of documents with CHUNK_SIZE = 500
+        if len(documents) == CHUNK_SIZE or index == len(json_data) - 1:
+            # Perform bulk indexing with the current chunk
+            bulk(es_client, documents, index=INDEX_NAME, chunk_size = CHUNK_SIZE)
+            total_index += len(documents)
+            print(f"Indexed {total_index} documents so far.")
+            documents = []
+
+print(create_index())
+insert_sudo_data()
