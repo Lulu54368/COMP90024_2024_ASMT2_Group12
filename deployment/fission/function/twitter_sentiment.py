e3256e307c1a85dc2ad45a54cfff3d7e4b247da6
Lulu Lu <luyuntao2019@gmail.com>
2024-05-14 10:06:41 +1000

    twitter sentiment

---
+++ deployment/fission/function/twitter_sentiment.py
@@ -0,0 +1,18 @@
+from flask import request, current_app
+import requests, logging
+import json
+def config(k):
+    with open(f'/configs/default/shared-data/{k}', 'r') as f:
+        return f.read()
+
+def config_twitter(k):
+    with open(f'/configs/default/twitter-data/{k}', 'r') as f:
+        return f.read()
+def main():
+    current_app.logger.info(f'Received request: ${request.headers}')
+    r = requests.get(config_twitter('TWITTER_URL'),
+        verify=False,
+        auth=(config('ES_USERNAME'), config('ES_PASSWORD')),
+        json= json.loads(config_twitter('TWITTER_REQ_BODY_AVG_SENTIMENT')))
+    current_app.logger.info(f'Status ES request: {r.status_code}')
+    return r.json()
