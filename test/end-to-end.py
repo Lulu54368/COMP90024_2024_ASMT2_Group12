09b27ea578a00b5ee61ead675a6f344974ce9ecd
Lulu Lu <luyuntao2019@gmail.com>
2024-05-21 13:53:51 +1000

    test

--- test/end-to-end.py
+++ test/end-to-end.py
@@ -22,27 +22,38 @@ class HTTPSession:
 
 class TestEnd2End(unittest.TestCase):
     def test_mastodon(self):
+        # Testing the mastodon (sentiment greater than 0)
         self.assertEqual(test_request.get('/mastodon/gt').status_code, 200)
         self.assertIsNotNone(test_request.get('/mastodon/gt').json())
-
+        self.assertGreater(len(test_request.get('/mastodon/gt').json()), 0)
+        # Testing the mastodon (sentiment less than 0)
         self.assertEqual(test_request.get('/mastodon/lt').status_code, 200)
         self.assertIsNotNone(test_request.get('/mastodon/lt').json())
-
+        self.assertGreater(len(test_request.get('/mastodon/lt').json()), 0)
+        # Testing the mastodon count
         self.assertEqual(test_request.get('/mastodon/count').status_code, 200)
         self.assertIsNotNone(test_request.get('/mastodon/count').json())
+        
 
     def test_twitter(self):
+        # Testing twitter (sentiment less than 0)
         self.assertEqual(test_request.get('/twitter/lt').status_code, 200)
         self.assertIsNotNone(test_request.get('/twitter/lt').json())
-
+        self.assertGreater(len(test_request.get('/twitter/lt').json()), 0)
+        # Testing twitter (sentiment greater than 0)
         self.assertEqual(test_request.get('/twitter/gt').status_code, 200)
         self.assertIsNotNone(test_request.get('/twitter/gt').json())
-
+        self.assertGreater(len(test_request.get('/twitter/gt').json()), 0)
+        # Testing count of twitter
         self.assertEqual(test_request.get('/twitter/count').status_code, 200)
         self.assertIsNotNone(test_request.get('/twitter/count').json())
-
+        self.assertEqual(int(test_request.get('/twitter/count').json()["count"]), 71566)
+        # Testing average sentiment score by area
         self.assertEqual(test_request.get('/twitter/sentiment').status_code, 200)
         self.assertIsNotNone(test_request.get('/twitter/sentiment').json())
+        self.assertGreater(len(test_request.get('/twitter/sentiment').json()), 0)
+
+
 
 
 if __name__ == '__main__':
